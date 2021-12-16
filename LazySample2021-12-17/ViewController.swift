98c39f1146c62a369c33c6e851bf696835d4fb0c
ryoryoryoooo <P1211065@gmail.com>
2021-12-17 07:42:58 +0900

    Initial Commit

---
+++ LazySample2021-12-17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LazySample2021-12-17
+//
+//  Created by 村中令 on 2021/12/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
