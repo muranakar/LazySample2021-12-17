6831e6efa70e11330d4a857491be6d47e8a185b2
ryoryoryoooo <P1211065@gmail.com>
2021-12-17 08:07:06 +0900

    playground lazyサンプル完成

--- LazySample2021-12-17/MyPlayground.playground/Contents.swift
+++ LazySample2021-12-17/MyPlayground.playground/Contents.swift
@@ -1,3 +1,46 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+
+
+struct Person {
+   let name: String
+   let age: Int
+}
+
+struct PeopleViewModel {
+    var people: [Person]
+    init(people: [Person]) {
+        self.people = people
+        print("View model initialized")
+    }
+ // lazy
+   lazy var oldest1: Person? = {
+       print("Lazy var oldest initialized")
+       return people.max(by: { $0.age < $1.age })
+   }()
+ // computed
+    var oldest2: Person? {
+         print("oldest person calculated")
+         return people.max(by: { $0.age < $1.age })
+     }
+
+}
+
+var viewModel = PeopleViewModel(people: [
+   Person(name: "Antoine", age: 30),
+   Person(name: "Jaap", age: 3),
+   Person(name: "Maaike", age: 27)
+])
+// lazy
+print(viewModel.oldest1!)
+// computed
+print(viewModel.oldest2!)
+
+viewModel.people.append(Person(name: "Jan", age: 69))
+// lazy
+print(viewModel.oldest1!)
+// computed
+print(viewModel.oldest2!)
+
